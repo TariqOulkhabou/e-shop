fd9bd91eedba29202ab6e428c66fd120077aa3f1
tarik <tarik123@gmail.com>
2023-08-08 11:54:17 +0100

    Brief description of the changes

--- src/app/Config/index.ts
+++ src/app/Config/index.ts
@@ -1,4 +1,4 @@
 export class Config {
     static readonly title: string = 'E-shop';
-    static readonly Api: string = '';
+    static readonly Api: string = 'https://fakestoreapi.com/products';
 }
--- src/app/Views/Pages/home/home.component.ts
+++ src/app/Views/Pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { ProductService } from 'src/app/Services/product/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  myData :any = [];
+  constructor(private service:ProductService){}
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.getProduct();
+  }
+  
+  getProduct(){
+    this.service.getAll().subscribe((data)=>{
+      this.myData = data
+      console.log(this.myData);
+    })
+  }
 
 }
+   
